ad8be5bdcb500b09f5df407af244b14cfda4023d
kybup1 <kybup1@bfh.ch>
2019-04-20 13:13:24 +0200

    Added Event Model Added String and Code to measurement Updated loadData to generate testdata using the date now()

--- server/boot/loadData.js
+++ server/boot/loadData.js
@@ -41,10 +41,18 @@ module.exports = function(app, cb) {
       let misterTestLinkedGeraet = new app.models.LinkedSensor({
         "altPatIdentifier": "1234",
         "sensor": sensor.sensorIdentifier,
-        "from": "2019-04-15T08:00Z",
+        "from": subtractFromNow(1, 3, 0, 0),
         "isConnected": true,
         "dataAccess": {},
       })
+      misterTestLinkedGeraet.savedData = misterTestLinkedGeraet.savedData.concat([
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 30, 50),"value":"80","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 30, 5),"value":"85","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 28, 20),"value":"90","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 27, 30),"value":"91","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 36, 40),"value":"90","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(1, 1, 26, 0),"value":"86","measurementString":"Pulse"},
+      ])
       misterTest.linkedSensors.push(misterTestLinkedGeraet)
       return misterTest
     }).then(misterTest =>{
@@ -55,7 +63,51 @@ module.exports = function(app, cb) {
       })
     })
 
+
+    app.models.Patient.create({
+      "patId":"10",
+      "firstname":"Hans",
+      "lastname":"Muster",
+      "birthdate":new Date("1966-03-20")
+    }).then(testPat2 => {
+      let TestPat2LinkedSensor = new app.models.LinkedSensor({
+        "altPatIdentifier": "1234",
+        "sensor": sensor.sensorIdentifier,
+        "from": subtractFromNow(10, 3, 0, 0),
+        "to":subtractFromNow(9, 0, 0, 0),
+        "isConnected": false,
+        "dataAccess": {},
+      })
+      TestPat2LinkedSensor.savedData = TestPat2LinkedSensor.savedData.concat([
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 30, 50),"value":"80","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 30, 5),"value":"85","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 28, 20),"value":"90","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 27, 30),"value":"91","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 36, 40),"value":"90","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 26, 0),"value":"86","measurementString":"Pulse"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 30, 50),"value":"96","measurementString":"Sauerstoffsättigung"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 30, 5),"value":"98","measurementString":"Sauerstoffsättigung"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 28, 20),"value":"97","measurementString":"Sauerstoffsättigung"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 27, 30),"value":"98","measurementString":"Sauerstoffsättigung"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 36, 40),"value":"95","measurementString":"Sauerstoffsättigung"},
+        {"sensor":"everion-1","timestamp":subtractFromNow(10, 1, 26, 0),"value":"98","measurementString":"Sauerstoffsättigung"}
+      ])
+      testPat2.linkedSensors.push(TestPat2LinkedSensor)
+      return testPat2
+    }).then(testPat2 =>{
+      testPat2.save()
+    })
+
   }).catch(err => {console.log(err)})
 
   process.nextTick(cb); // Remove if you pass `cb` to an async function yourself
 };
+
+let subtractFromNow = function(days, hours, minutes, seconds) {
+  let date = new Date();
+  date.setDate(date.getDate() - days);
+  date.setHours(date.getHours() - hours);
+  date.setMinutes(date.getMinutes() - minutes);
+  date.setSeconds(date.getSeconds() -seconds);
+  return date;
+}
