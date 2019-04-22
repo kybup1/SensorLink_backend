38d8de7d165f6a2bf3c2e4ccad6dfef6d5f2bdb6
kybup1 <kybup1@bfh.ch>
2019-04-22 09:18:36 +0200

    Added Patient GET request to retrieve patients with linked sensors  at the moment and at a specific date

--- common/models/patient.js
+++ common/models/patient.js
@@ -2,4 +2,46 @@
 
 module.exports = function(Patient) {
 
+  Patient.withConnectedSensor = function(cb) {
+    Patient.find().then((patients) => {
+      let selectedPatients = [];
+      patients.forEach(pat => {
+        let addPatient = false;
+        pat.linkedSensors.forEach(ls => {
+          if (ls.isConnected == true) {
+            addPatient = true;
+          }
+        });
+
+        if(addPatient == true){
+          selectedPatients.push(pat);
+        }
+
+      });
+      cb(null, selectedPatients);
+    });
+  };
+
+  Patient.withConnectedSensorAt = function(from, to, cb) {
+    Patient.find().then((patients) => {
+      let selectedPatients = [];
+      patients.forEach(pat => {
+        let addPatient = false;
+        pat.linkedSensors.forEach(ls => {
+          if (ls.isConnected == true && ls.from.setHours(0, 0, 0, 0) <= from) {
+            addPatient = true;
+          } else if (ls.to && ls.from.setHours(0, 0, 0, 0) >= from.setHours(0, 0, 0, 0) && ls.to.setHours(0, 0, 0, 0) <= to.setHours(0, 0, 0, 0)) {
+            addPatient = true;
+          }
+        });
+
+        if(addPatient == true){
+          selectedPatients.push(pat);
+        }
+
+      });
+      cb(null, selectedPatients);
+    });
+  };
+
 };
